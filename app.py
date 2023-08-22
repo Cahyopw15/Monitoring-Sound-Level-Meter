63eb99e3a028761e9f2a19d5f1a4a01e087ba4f1
Cahyopw15 <141690644+Cahyopw15@users.noreply.github.com>
2023-08-22 15:42:39 +0700

    Update app.py

--- app.py
+++ app.py
@@ -8,8 +8,7 @@ from supabase import create_client, Client
 import json
 
 st.image("Logo_UnivLampung.png", width = 50)
-config_url = config.toml
-st.write(config_url)
+
 @st.cache_resource
 def init_connection():
     url = st.secrets["supabase_url"]
@@ -26,7 +25,8 @@ def convert_dict_to_df(data):
     
 st.title("Real-Time Monitoring Kebisingan Dashboard")
 
-st.sidebar.header('Dashboard Monitoring Kebisingan')
+
+
 placeholder = st.empty()    
 while True:
         supabase = init_connection()
@@ -55,10 +55,10 @@ while True:
                     for k in range(4):
                         fig.add_annotation(x=j, y=k, text=str(z[j,k]), showarrow=False, font_size=10, font_color='black')
                 fig.update_layout(margin=dict(l=8, r=8, b=8, pad=8), plot_bgcolor='rgba(0,0,0,0)', paper_bgcolor='rgba(0,0,0,0)')
-                st.plotly_chart(fig, theme="streamlit") 
+                
                 fig_surface = go.Figure(data=[go.Surface(z=z, colorscale=colorscale)])
                 fig_surface.update_layout(width=500, height=500, autosize=False, margin=dict(l = 65 ,  r = 50 ,  b = 65 ,  pad = 90), plot_bgcolor='rgba(0,0,0,0)', paper_bgcolor='rgba(0,0,0,0)')
-                st.plotly_chart(fig_surface, theme="streamlit")
+                
         else:
             with placeholder.container():
                 st.write("Not Enough Data!")
@@ -66,4 +66,11 @@ while True:
 
 
 st.sidebar.header('Dashboard Monitoring Kebisingan')
+col1,col2 = st.columns(2)
+with col1:
+    st.markdown("### Topografi 2D")
+    st.plotly_chart(fig, theme="streamlit") 
 
+with col2: 
+     st.markdown("### Topografi 3D")
+     st.plotly_chart(fig_surface, theme="streamlit")
