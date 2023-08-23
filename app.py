965feaebf3ad051509ed9e6140ba9be62f631542
Cahyopw15 <141690644+Cahyopw15@users.noreply.github.com>
2023-08-23 11:22:44 +0700

    Update app.py

--- app.py
+++ app.py
@@ -80,7 +80,9 @@ elif option == 'About':
     st.write("[![Star](https://img.shields.io/github/stars/dataprofessor/links.svg?logo=github&style=social)](https://gitHub.com/dataprofessor/links)")
     col1, col2, col3 = st.columns(3)
     col2.image(Image.open('dp.png'))
+    
     st.header('Chanin Nantasenamat, Ph.D.')
+    
     st.info('Developer Advocate, Content Creator and ex-Professor with an interest in Data Science and Bioinformatics')
         
     icon_size = 20
