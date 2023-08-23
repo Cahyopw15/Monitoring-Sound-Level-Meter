df897f925c20579def9360083189d0cc2db8dd47
Cahyopw15 <141690644+Cahyopw15@users.noreply.github.com>
2023-08-23 11:36:49 +0700

    Update st_functions.py

--- st_functions.py
+++ st_functions.py
@@ -71,7 +71,7 @@ def st_button(icon, url, label, iconsize):
         #button_code = f'''
         #<p>
             #<a href={url} class="btn btn-outline-primary btn-lg btn-block" type="button" aria-pressed="true">
-                {label}
+                #{label}
             #</a>
         #</p>'''
     
